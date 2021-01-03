54e1539f449d24ec4be767180b5aeae2844d4b0c
kko0831 <kko0831@hanmail.net>
2021-01-03 17:11:40 +0900

    3.5 Mapped Types

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,8 @@
-import { Field, ObjectType, Int } from "@nestjs/graphql";
+import { Field, InputType, Int, ObjectType } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class Restaurant {
@@ -10,21 +12,27 @@ export class Restaurant {
 
   @Field(() => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
   @Field(() => Boolean)
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field(() => String)
   @Column()
+  @IsString()
   ownersName: string;
 
   @Field(() => String)
   @Column()
+  @IsString()
   categoryName: string;
 }
